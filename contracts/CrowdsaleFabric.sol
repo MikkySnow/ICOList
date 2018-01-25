aaf61378012d8a0711c6e247744bf80bb6f718c9
mike <mikkysnowman@gmail.com>
2018-01-26 01:08:02 +0300

    Initial commit

--- contracts/CrowdsaleFabric.sol
+++ contracts/CrowdsaleFabric.sol
@@ -1,8 +1,8 @@
 pragma solidity ^0.4.18;
 
 /**
-*   Fabric for crowdsales. There're not common interface for crowdsale, so
-*   we should use fabric to avoid it. Also by using fabric we can support interactive crowdsales
+*   Fabric for crowdsale. There're not common interface for crowdsale, so
+*   we should use fabric to avoid it. Also by using fabric we can support interactive crowdsale
 **/
 
 contract CrowdsaleFabric {
