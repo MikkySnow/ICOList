6209854fb686395ddfcfd05f50e1f3b094bb1034
mike <mikkysnowman@gmail.com>
2018-01-26 16:57:23 +0300

    Added mapping and array for crowdsales

--- contracts/CrowdsaleStorage.sol
+++ contracts/CrowdsaleStorage.sol
@@ -15,9 +15,11 @@ contract CrowdsaleStorage {
 
     /*** STORAGE ***/
 
-    // Mapping for all crowdsales. One address can create multiple crowdsales,
-    // so we should use uint256 index
-    mapping (uint256 => CrowdsaleInfo) crowdsales;
+    // An array where stored all crowdsales
+    CrowdsaleInfo[] crowdsales;
+
+    // A mapping from crowdsale index to address that owns it
+    mapping (uint256 => address) indexToOwner;
 
     /*** EVENTS ***/
 
@@ -32,7 +34,12 @@ contract CrowdsaleStorage {
     *   @param _crowdsaleAddress            address of crowdsale contract
     *   @param _tokenAddress                address of token contract
     **/
-    function addCrowdsale(address _crowdsaleAddress, address _tokenAddress) internal {
+    function addCrowdsale(address _crowdsaleAddress, address _tokenAddress, address _owner) internal {
+        CrowdsaleInfo memory _crowdsale = CrowdsaleInfo(_crowdsaleAddress, _tokenAddress);
+
+        uint256 index = crowdsales.push(_crowdsale) - 1;
+        indexToOwner[index] = _owner;
+
         CrowdsaleWasAdded(_crowdsaleAddress, _tokenAddress);
     }
 }
