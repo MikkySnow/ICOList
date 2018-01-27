992286b3a66adf0c705147c0ab1bb8bc80fc7d20
mike <mikkysnowman@gmail.com>
2018-01-27 13:20:03 +0300

    Initial commit

---
+++ contracts/Management.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.18;
+
+/**
+*   @title Contract that implements logic of management.
+*   @dev Implement logic of multisig, interaction with other contracts and
+*   role management
+**/
+
+contract Management {
+
+    function Management(){
+
+    }
+}
