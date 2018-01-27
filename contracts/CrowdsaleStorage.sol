6d8a4ab26f9a375e21c3027e65a3df4d488b69aa
mike <mikkysnowman@gmail.com>
2018-01-27 12:50:54 +0300

    Documenting comments added

--- contracts/CrowdsaleStorage.sol
+++ contracts/CrowdsaleStorage.sol
@@ -7,14 +7,14 @@ pragma solidity ^0.4.18;
 **/
 contract CrowdsaleStorage {
 
-    // Enum for crowdsale status
+    /// @dev Enum for crowdsale status
     enum CrowdsaleStatus {
         Waiting,        // For crowdsales, which waiting for acceptance from admins
         Active,         // For crowdsales, which is active at moment. At moment can be only 1 active crowdsale
         Ended           // For crowdsales, which is gone. They cannot be restarted
     }
 
-    // Basic struct for crowdsale info
+    /// @dev Basic struct for crowdsale info
     struct CrowdsaleInfo {
         address crowdsaleAddress;
         address tokenAddress;
@@ -23,27 +23,32 @@ contract CrowdsaleStorage {
 
     /*** STORAGE ***/
 
-    // An array where stored all crowdsales
+    /// @dev An array where stored all crowdsales
     CrowdsaleInfo[] crowdsales;
 
-    // A mapping from crowdsale index to address that owns it
+    /// @dev A mapping from crowdsale index to address that owns it
     mapping (uint256 => address) indexToOwner;
 
     /*** EVENTS ***/
 
-    // Emits when crowdsale was added to storage
+    /// @dev Emits when crowdsale was added to storage
     event CrowdsaleWasAdded(address _crowdsaleAddress, address _tokenAddress);
 
     /*** FUNCTIONS ***/
 
     /**
-    *   Adds crowdsale info to storage. Calls only by Crowdsale fabric contract
+    *   @dev Adds crowdsale info to storage.
+    *   Calls internal by Crowdsale fabric contract
     *   in createCrowdsale() function
     *   @param _crowdsaleAddress            address of crowdsale contract
     *   @param _tokenAddress                address of token contract
     **/
-    function addCrowdsale(address _crowdsaleAddress, address _tokenAddress, address _owner) internal {
-        CrowdsaleInfo memory _crowdsale = CrowdsaleInfo(_crowdsaleAddress, _tokenAddress);
+    function _addCrowdsale(address _crowdsaleAddress, address _tokenAddress, address _owner) internal {
+        CrowdsaleInfo memory _crowdsale = CrowdsaleInfo({
+            crowdsaleAddress: _crowdsaleAddress,
+            tokenAddress: _tokenAddress,
+            status: CrowdsaleStatus.Waiting
+            });
 
         uint256 index = crowdsales.push(_crowdsale) - 1;
         indexToOwner[index] = _owner;
