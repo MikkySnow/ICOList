ad09d585452cbcfd91daeae84002a61c9aeb4318
mike <mikkysnowman@gmail.com>
2018-01-27 22:11:24 +0300

    Initial commit

---
+++ contracts/BusinessLogic.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.18;
+
+import "zeppelin-solidity/contracts/lifecycle/Pausable.sol";
+
+/**
+*   @title Contract for business logic
+*   @dev There implemented all business logic
+*/
+contract BusinessLogic is Pausable {
+
+}
