736fcea901152cb6cb075cb34e187c3f11b46942
mike <mikkysnowman@gmail.com>
2018-01-31 11:12:10 +0300

    Added logic for voting for money withdrawal

--- contracts/BusinessLogic.sol
+++ contracts/BusinessLogic.sol
@@ -25,6 +25,23 @@ contract BusinessLogic is Management {
     /// @dev How much ether needs to create crowdsale
     uint256 constant public CROWDSALE_FEE = 1;
 
+    /*** STORAGE ***/
+
+    /**
+     *  Amounts of ether for each address
+     */
+    mapping (address => uint256) investedAmount;
+
+    /**
+     *  Number of admins which signed withdraw proposal
+     */
+    uint8 numberOfSigns;
+
+    /**
+     *  An array for signedAddresses
+     */
+    address[] signedAdmins;
+
     /*** FUNCTIONS ***/
 
     /**
@@ -33,8 +50,11 @@ contract BusinessLogic is Management {
     *   @param _address         Address to withdraw
     *   @param _amount          Amount to withdraw
     **/
-    function withdraw(address _address, uint256 _amount) {
+    function withdraw(address _address, uint256 _amount) internal {
+        require(this.balance >= _amount);
+        _address.send(_amount);
 
+        MoneyWithdrawal(_amount, _address);
     }
 
     /**
@@ -49,7 +69,7 @@ contract BusinessLogic is Management {
     *   @param _token           Address of crowdsale token
     **/
     function createCrowdsale(uint256 _start, uint256 _end, uint256 _rate, uint256 _goal, uint256 _cap, address _wallet, address _token)
-    public whenNotPaused payable {
+    public whenNotPaused payable onlyAdmins {
 
     }
 
@@ -57,7 +77,7 @@ contract BusinessLogic is Management {
     *   @dev Function for crowdsale contributing by users
     *   Contributing of crowdsale takes some fees
     **/
-    function contributeCrowdsale() payable whenNotPaused public {
+    function contribute() payable whenNotPaused public {
 
     }
 
@@ -70,9 +90,91 @@ contract BusinessLogic is Management {
     }
 
     /**
+     *  @dev Function for claiming tokens if crowdsale was successful
+     */
+    function claimTokens() {
+
+    }
+
+    /**
      *   @dev Overrides disallowing function to receive ether
      */
     function() public payable {
+    }
 
+    /**
+     *  @dev Checks if address already signed proposal
+     */
+    function isSigned(address _address) public constant returns (bool) {
+
+        for (uint8 i=0; i<signedAdmins.length-1; i++) {
+            if (_address == signedAdmins[i]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *  @dev Accepts money withdrawal
+     *  Money withdrawal starts when at least 3 admins signed
+     *  @param _address         Address to withdraw
+     *  @param _amount          Amount to withdraw
+     */
+    function acceptWithdrawal(uint256 _amount, address _address) onlyAdmins {
+        // One person can't vote twice
+        require(!isSigned(msg.sender));
+        // We cannot withdraw to zero address
+        require(_address != 0x0);
+        // We cannot withdraw more than we have
+        require(_amount <= this.balance);
+
+        // Check if there more than 70000 gas
+        require(msg.gas < 70000);
+
+        // Votes for withdrawal
+        numberOfSigns++;
+        // Sets that msg.sender already voted
+        signedAdmins[signedAdmins.length] = msg.sender;
+
+        // Checks if we ready to start withdrawal
+        if (numberOfSigns >= 3) {
+            withdraw(_address, _amount);
+            // If withdraw starts we clear list of signed admins
+            clearSignedAdminsList();
+        }
+    }
+
+    /**
+     *  @dev Decline money withdrawal
+     */
+    function declineWithdrawal() onlyAdmins {
+        // One person can't vote twice
+        require(!isSigned(msg.sender));
+
+        // Check if number of signs is greater than zero
+        require(numberOfSigns > 0);
+
+        // Check if there more than 70000 gas
+        require(msg.gas < 70000);
+
+        // Decline one vote for withdrawal
+        numberOfSigns--;
+
+        // Sets that msg.sender already voted
+        signedAdmins[signedAdmins.length] = msg.sender;
+
+        // If number of signs is zero, we clear list of signed admins
+        clearSignedAdminsList();
+    }
+
+    /**
+     *  @dev Clears list of signed admins
+     */
+    function clearSignedAdminsList() internal {
+        for (uint8 i=0; i<signedAdmins.length-1; i++) {
+            delete signedAdmins[i];
+        }
     }
 }
