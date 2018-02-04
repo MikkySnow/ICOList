092069f93fe4a81b8776b7e497b82fd435754029
mike <mikkysnowman@gmail.com>
2018-02-04 18:00:59 +0300

    Added voting mechanism for adding crowdsale storage address

--- contracts/BusinessLogic.sol
+++ contracts/BusinessLogic.sol
@@ -20,8 +20,6 @@ contract BusinessLogic is Management {
 
     /*** EVENTS ***/
 
-    /// @dev Emits when somebody withdraws money from contract
-    event MoneyWithdrawal(uint256 _amount, address _address);
 
     /// @dev Emits when new MoneyVault address was set
     event NewMoneyVaultAddress(address _address, address _setBy);
@@ -41,31 +39,8 @@ contract BusinessLogic is Management {
     /// @dev Storage where all crowdsales stored
     CrowdsaleStorage crowdsaleStorage;
 
-    /**
-     *  Number of admins which signed withdraw proposal
-     */
-    uint8 numberOfSigns;
-
-    /**
-     *  An array for signedAddresses
-     */
-    address[] signedAdmins;
-
 
     /*** FUNCTIONS ***/
-
-    /**
-    *   @dev Function for money withdrawal
-    *   Can be called only by 2 or more admins
-    *   @param _address         Address to withdraw
-    *   @param _amount          Amount to withdraw
-    **/
-    function withdraw(address _address, uint256 _amount) internal {
-        require(this.balance >= _amount);
-        _address.transfer(_amount);
-
-        MoneyWithdrawal(_amount, _address);
-    }
 
     /**
     *   @dev Function for crowdsale contributing by users
@@ -73,9 +48,7 @@ contract BusinessLogic is Management {
     *   User money sends to MoneyVault
     **/
     function contribute() payable whenNotPaused public {
-        uint256 amount = msg.value.mul(CONTRIBUTE_FEE).div(100);
-        moneyVaultAddress.transfer(amount);
-        MoneyVault(moneyVaultAddress).deposit(msg.sender, amount);
+
     }
 
     /**
@@ -93,10 +66,10 @@ contract BusinessLogic is Management {
 
         // Getting how much tokens user should get
         Crowdsale crowdsale = Crowdsale(crowdsaleAddress);
-        uint256 amount = crowdsale.rate;
+//        uint256 amount = crowdsale.rate;
 
         // Transfer tokens to user
-        token.transfer(msg.sender, amount);
+//        token.transfer(msg.sender, amount);
     }
 
     /**
@@ -105,84 +78,10 @@ contract BusinessLogic is Management {
      */
     function() public payable {
         uint256 amount = msg.value.mul(CONTRIBUTE_FEE).div(100);
-        moneyVaultAddress.call.value(amount).gas(25000)(bytes4(sha3("deposit(address)")), msg.sender);
+
     }
 
-    /**
-     *  @dev Checks if address already signed proposal
-     */
-    function isSigned(address _address) public constant returns (bool) {
 
-        for (uint8 i=0; i<signedAdmins.length-1; i++) {
-            if (_address == signedAdmins[i]) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    /**
-     *  @dev Accepts money withdrawal
-     *  Money withdrawal starts when at least 3 admins signed
-     *  @param _address         Address to withdraw
-     *  @param _amount          Amount to withdraw
-     */
-    function acceptWithdrawal(uint256 _amount, address _address) onlyAdmins public {
-        // One person can't vote twice
-        require(!isSigned(msg.sender));
-        // We cannot withdraw to zero address
-        require(_address != 0x0);
-        // We cannot withdraw more than we have
-        require(_amount <= this.balance);
-
-        // Check if there more than 70000 gas
-        require(msg.gas < 70000);
-
-        // Votes for withdrawal
-        numberOfSigns++;
-        // Sets that msg.sender already voted
-        signedAdmins[signedAdmins.length] = msg.sender;
-
-        // Checks if we ready to start withdrawal
-        if (numberOfSigns >= 3) {
-            withdraw(_address, _amount);
-            // If withdraw starts we clear list of signed admins
-            clearSignedAdminsList();
-        }
-    }
-
-    /**
-     *  @dev Decline money withdrawal
-     */
-    function declineWithdrawal() onlyAdmins public {
-        // One person can't vote twice
-        require(!isSigned(msg.sender));
-
-        // Check if number of signs is greater than zero
-        require(numberOfSigns > 0);
-
-        // Check if there more than 70000 gas
-        require(msg.gas < 70000);
-
-        // Decline one vote for withdrawal
-        numberOfSigns--;
-
-        // Sets that msg.sender already voted
-        signedAdmins[signedAdmins.length] = msg.sender;
-
-        // If number of signs is zero, we clear list of signed admins
-        clearSignedAdminsList();
-    }
-
-    /**
-     *  @dev Clears list of signed admins
-     */
-    function clearSignedAdminsList() internal {
-        for (uint8 i=0; i<signedAdmins.length-1; i++) {
-            delete signedAdmins[i];
-        }
-    }
 
     /**
      *  @dev Sets MoneyVault contract address
@@ -199,14 +98,34 @@ contract BusinessLogic is Management {
 
     /**
      *  @dev Sets CrowdsaleStorage contract address
+     *  Can be called only by more than 2 admins
      *  @param _address         New address of CrowdsaleStorage contract
      */
     function setCrowdsaleStorageAddress(address _address) onlyAdmins whenPaused public {
         // We cannot set contract address to zero
         require(_address != 0x0);
-        // Sets new address
-        crowdsaleStorage = CrowdsaleStorage(_address);
-        // Emits event
-        NewCrowdsaleStorageAddress(_address, msg.sender);
+
+        if (adminCount == 1) {
+            // Sets new address
+            crowdsaleStorage = CrowdsaleStorage(_address);
+            // Emits event
+            NewCrowdsaleStorageAddress(_address, msg.sender);
+        } else {
+            // Checks if there enough votes
+            if (proposals[_address].votesNumber > 2) {
+                // Sets new address
+                crowdsaleStorage = CrowdsaleStorage(_address);
+                // Emits event
+                NewCrowdsaleStorageAddress(_address, msg.sender);
+                // Remove proposal
+                delete proposals[_address];
+            } else {
+                // Checks if admin already voted
+                if (!isAlreadyVoted(msg.sender, _address)) {
+                    proposals[_address].votesNumber++;
+                    proposals[_address].votes.push(msg.sender);
+                }
+            }
+        }
     }
 }
