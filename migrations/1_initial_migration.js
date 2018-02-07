2c3ac319de74fdc7269150ff40ee663f51ed4640
mike <mikkysnowman@gmail.com>
2018-02-07 12:54:46 +0300

    Initial commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,9 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var AdminMoneyVault = artifacts.require("./AdminMoneyVault.sol");
+var BusinessLogic = artifacts.require("./BusinessLogic.sol");
+var Management = artifacts.require("./Management.sol");
+var MoneyVault = artifacts.require("./MoneyVault.sol");
+var CrowdsaleStorage = artifacts.require("./CrowdsaleStorage.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
