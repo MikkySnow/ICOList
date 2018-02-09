dd35fe61fac08aaa32a706640ab48590a9299fa1
mike <mikkysnowman@gmail.com>
2018-02-09 16:28:06 +0300

    Removed unused

---
+++ contracts/ERC20.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.4.19;
+
+// @title Interface for interaction with ERC20 and ERC223 Tokens
+contract ERC20 {
+  function balanceOf(address who) public view returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+}
