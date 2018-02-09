e502073a6eaea06382fcd0260b0058aba265bc10
mike <mikkysnowman@gmail.com>
2018-02-09 10:30:18 +0300

    Ready contract

--- contracts/AdminMoneyVault.sol
+++ contracts/AdminMoneyVault.sol
@@ -4,8 +4,7 @@ import "./Management.sol";
 
 /**
  *   @title A vault for admins funds
- *   @dev There stored all admins ether,
- *   earned by taking fees. Only admins can use this contract
+ *   @dev There stored all admins ether earned by taking fees
  */
 contract AdminMoneyVault is Management {
 
@@ -20,7 +19,7 @@ contract AdminMoneyVault is Management {
     *   @param _address         Address to withdraw
     *   @param _amount          Amount to withdraw
     **/
-    function withdraw(address _address, uint256 _amount) onlyAdmins external {
+    function withdraw(address _address, uint256 _amount) onlyCFO external {
         // Checks if contract balance greater than withdrawal amount
         require(this.balance >= _amount);
         // Transfers ether to _address
@@ -30,7 +29,7 @@ contract AdminMoneyVault is Management {
     }
 
     // @dev Default constructor for BusinessLogic contract
-    // @param _address Address of Interactions contract
+    // @param _address Address of Ownership contract
     function AdminMoneyVault(address _address) Management(_address) public {
         require(_address != 0x0);
     }
