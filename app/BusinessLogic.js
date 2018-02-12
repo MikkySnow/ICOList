497ea99085b4bd48e36a49fbca8e9a8eb9e723bd
mike <mikkysnowman@gmail.com>
2018-02-12 11:12:03 +0300

    Init

---
+++ app/BusinessLogic.js
@@ -0,0 +1,83 @@
+// Import libraries we need
+import { default as Web3} from 'web3';
+import { default as contract } from 'truffle-contract';
+
+// Import our contract artifacts and turn them to usable abstraction
+import BusinessLogicArtifacts from '../build/contracts/BusinessLogic.json';
+
+// Creates usable abstractions
+const BusinessLogic = contract(BusinessLogicArtifacts);
+
+let account;    // Stores user account between calls
+
+window.App = {
+    // Initializing function
+    start: function () {
+        BusinessLogic.setProvider(web3.currentProvider);
+
+        // Get the initial account balance so it can be displayed.
+        web3.eth.getAccounts(function(err, accs) {
+            if (err !== null) {
+                alert('Log in your MetaMask account')
+                return
+            }
+
+            if (accs.length === 0) {
+                alert('Cannot get accounts from MetaMask')
+                return
+            }
+            account = accs[0]
+        })
+    },
+
+    setCrowdsaleStorage: function (address) {
+        BusinessLogic.deployed().then(function (instance) {
+            instance.setCrowdsaleStorage(address, {from: account})
+        }).catch(function (e) {
+            console.log(e)
+        })
+    },
+
+    setAdminMoneyVault: function (address) {
+        BusinessLogic.deployed().then(function (instance) {
+            instance.setAdminMoneyVault(address, {from: account})
+        }).catch(function (e) {
+            console.log(e)
+        })
+    },
+
+    pause: function () {
+        BusinessLogic.deployed().then(function (instance) {
+            instance.pause({from: account});
+        }).catch(function (e) {
+            console.log(e)
+        })
+    },
+
+    unpause: function () {
+        BusinessLogic.deployed().then(function (instance) {
+            instance.unpause({from: account})
+        }).catch(function (e) {
+            console.log(e)
+        })
+    },
+
+    setFees: function (amount) {
+        BusinessLogic.deployed().then(function (instance) {
+            instance.setFee(amount)
+        }).catch(function (e) {
+            console.log(e)
+        })
+    }
+};
+
+window.addEventListener('load', function () {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        window.web3 = new Web3(web3.currentProvider)
+    } else {
+        alert('You should download MetaMask/Mist for security reasons')
+    }
+
+    App.start()
+});
--- app/app.js
+++ app/app.js
@@ -1,41 +1,31 @@
+// Import libraries we need
 import { default as Web3} from 'web3';
 import { default as contract } from 'truffle-contract';
 
+// Import our contract artifacts and turn them to usable abstraction
 import CrowdsaleStorageArtifacts from '../build/contracts/CrowdsaleStorage.json';
 import AdminMoneyVaultArtifacts from '../build/contracts/AdminMoneyVault.json';
 import BusinessLogicArtifacts from '../build/contracts/BusinessLogic.json';
 import ManagementArtifacts from '../build/contracts/Management.json';
+import OwnershipArtifacts from '../build/contracts/Ownership.json';
 
 // Creates usable abstractions
 const CrowdsaleStorage = contract(CrowdsaleStorageArtifacts);
 const AdminMoneyVault = contract(AdminMoneyVaultArtifacts);
-const MoneyVault = contract(MoneyVaultArtifacts);
 const BusinessLogic = contract(BusinessLogicArtifacts);
 const Management = contract(ManagementArtifacts);
+const Ownership = contract(OwnershipArtifacts);
 
 let account;    // Stores user account between calls
 
-// Initialize accounts and check availability of Metamask
-async function start() {
-    CrowdsaleStorage.setProvider(web3.currentProvider);
-    AdminMoneyVault.setProvider(web3.currentProvider);
-    MoneyVault.setProvider(web3.currentProvider);
-    BusinessLogic.setProvider(web3.currentProvider);
-    Management.setProvider(web3.currentProvider);
+window.App = {
+    start: function () {
+        CrowdsaleStorage.setProvider(web3.currentProvider);
+        AdminMoneyVault.setProvider(web3.currentProvider);
+        BusinessLogic.setProvider(web3.currentProvider);
+        Management.setProvider(web3.currentProvider);
+        Ownership.setProvider(web3.currentProvider);
 
-    // Get the initial account
-    web3.eth.getAccounts(function (err, accs) {
-        if (err !== null) {
-            alert('Log in your MetaMask account')
-            return
-        }
 
-        if (accs.length === 0) {
-            alert('Cannot get accounts from MetaMask')
-            return
-        }
-
-        account = accs[0]
-    })
-
+    }
 }
